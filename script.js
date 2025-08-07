cf512cc7b39a30a5d161394840d7bc59b786ca82
Piyush Rai <114089436+akapiyushrai@users.noreply.github.com>
2025-08-07 13:35:15 +0530

    added

---
+++ script.js
@@ -0,0 +1,34 @@
+const passwordBox = document.getElementById("password");
+const generateBtn = document.getElementById("generateBtn");
+const copyBtn = document.getElementById("copyBtn");
+
+const length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@#$%^&*()_+~|}{[]></-=";
+
+const allChar = upperCase + lowerCase + number + symbol;
+
+function createPassword() {
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while (password.length < length) {
+        password += allChar[Math.floor(Math.random() * allChar.length)];
+    }
+
+    passwordBox.value = password;
+}
+
+function copyPassword() {
+    passwordBox.select();
+    document.execCommand("copy");
+}
+
+generateBtn.addEventListener("click", createPassword);
+copyBtn.addEventListener("click", copyPassword);
